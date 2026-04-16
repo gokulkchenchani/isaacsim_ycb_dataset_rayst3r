ca08a5eb4b5976463bfba18078f0eb3207d64b54
Gokul Krishna Chenchani <gchencha@uni-bonn.de>
2026-04-16 14:09:01 +0200

    add new ycb objects & update scripts

---
+++ ycb_scripts/ycb_objects_loading.py
@@ -0,0 +1,11 @@
+from omni.isaac.core.utils.stage import add_reference_to_stage
+import os
+
+base = "/home/user/chenchani/Desktop/thesis/ws_thesis/src/isaacsim_ycb_dataset_rayst3r/ycb_objects/isaac_objects/1_legacy"
+
+for obj in os.listdir(base):
+    usd = os.path.join(base, obj, "isaac_gt.usd")
+    if os.path.exists(usd):
+        prim_path = f"/World/isaac_objects/{obj}"   # ✅ unique name
+        add_reference_to_stage(usd, prim_path)
+        
